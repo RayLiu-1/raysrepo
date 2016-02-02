b39ee8012362f931cb22b5623a5aa4e283171ae1
Rui Liu <ruli7792@colorado.edu>
2016-02-02 02:30:17 -0700

    done

--- main.c
+++ main.c
@@ -1,29 +1,40 @@
 #include <stdio.h>
-#include <hw1.h>
+#include "hw1.h"
+
+
 int main(){
   printf("Hello World\n");
   printf("The strings before resversing\n");
-  char a* = "Yes";
-  char b* = "qweasd";
-  char c* = "!@3ijm";
+  char a[] = "Yes";
+  char b[] = "qweasd";
+  char c[] = "!@3ijm";
   printf(a);
   printf("\n");
   printf(b);
   printf("\n");  
   printf(c);
   printf("\n");
-  printf("The strings after resversing\n");  
-  printf(reverse_string(a));
+  printf("The strings after resversing\n");
+  reverse_string(a,3);
+  printf(a);
   printf("\n");
-  printf(reverse_string(b));
-  printf("\n");  
-  printf(reverse_string(c));
+  reverse_string(b,6);
+  printf(b);
   printf("\n");
-  printf(reverse_string("This is a string."));
+  reverse_string(c,6);
+  printf(c);
   printf("\n");
-  printf(reverse_string("some NUMmbers12345"));
-  printf("\n");  
-  printf(reverse_string("Does it reverse \n\0\t correctly?"));
+  char d[] = "This is a string.";
+  reverse_string(d,17);
+  printf(d);
+  printf("\n");
+  char e[] = "some NUMmbers12345";
+  reverse_string(e,18);
+  printf(e);
+  printf("\n");
+  char f[] = "Does it reverse \n\0\t correctly?";
+  reverse_string(f,31);
+  printf("f");
   printf("\n");
   return 0;
 }
